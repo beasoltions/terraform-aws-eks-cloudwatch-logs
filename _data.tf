428a8187d5cf4de1deffe4243f1716f8d3e6996b
deenMuhammad <zorro199696@gmail.com>
2024-06-22 19:51:29 +0900

    Add values

--- _data.tf
+++ _data.tf
@@ -1,3 +1,11 @@
 data "aws_iam_policy" "cloudwatch_logs" {
   arn = "arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy"
 }
+
+data "utils_deep_merge_yaml" "values" {
+  count = var.enabled ? 1 : 0
+  input = compact([
+    local.values_default,
+    var.values
+  ])
+}
